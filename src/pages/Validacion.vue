5f73f9e3ae6a633410158e9902d6cf2dd6a29456
vladimirjv <ivladimirjuarez@outlook.com>
2018-12-02 14:28:02 -0600

    new values

--- src/pages/Validacion.vue
+++ src/pages/Validacion.vue
@@ -17,6 +17,8 @@
           float-label="Frecuencia en MHz"
           placeholder="30, 53, 98, 123, 230, 290"
           color="green"
+          @input="validateFrq"
+          :error="errorFrq"
         />
       </div>
 
@@ -26,6 +28,8 @@
           float-label="Valores en dBuV"
           placeholder="23,14,18,9,21"
           color="green"
+          @input="validatePk"
+          :error="errorPk"
         />
       </div>
 
@@ -53,26 +57,58 @@ export default {
       width: 0,
       height:0,
       loading: false,
+      errorFrq:false,
+      errorPk:false,
+      dataG:{},
     }
-  },methods:{
+  },
+  methods:{
     progress(){
       this.loading=true;
       //split combierte el array separadon los valores por la ,
       //map function hace mappign sobre el array y transforma en number
       //de no ser un numero, se agreg como NaN
       var frq=this.frecuencia.split(",").map(Number)
-
-      setTimeout(()=>{
-        this.loading=false;
-      },3000)
+      console.log(this.validateArray(frq))
+      // this.$axios.put()
     },
-    simulatedlist(){console.log([123,1231,123]);},
+    validateFrq(){
+      var frq=this.frecuencia.split(",").map(Number)
+      this.errorFrq=!this.validateArray(frq)
+    },
+    validatePk(){
+      var pk=this.db.split(",").map(Number)
+      this.errorPk=!this.validateArray(pk)
+    },
+    validateArray(array){
+      var validated=array.find((val,index)=>{
+        return isNaN(val)
+      })
+      console.log(typeof validated)
+      switch (typeof(validated)) {
+        case "undefined":
+          return true
+          break;
+        case "number":
+          return false
+          break;
+      }
+    },
     ...mapActions({
       updateRuta: types.UPDATE_RUTA
     })
   },
+  computed: {
+  },
   mounted () {
     this.updateRuta(this.$router.currentRoute.fullPath);
+    this.$axios.get('https://floating-sea-76017.herokuapp.com/datasf').
+    then((response) => {
+      this.dataG=response.data
+      console.log(dataG)
+    }).catch((err) => {
+      console.log('something wrong hapend')
+    });
   }
 }
 </script>
